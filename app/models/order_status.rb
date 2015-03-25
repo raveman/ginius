ad0b9383dd08a0bd260b86cb282ea78f5c3dcbf1
bob ershov <raveman@raveman.ru>
2015-03-25 19:50:35 +0300

    updated order statuses

--- app/models/order_status.rb
+++ app/models/order_status.rb
@@ -1,11 +1,33 @@
 class OrderStatus < ActiveRecord::Base
 
 	def self.statuses
-		{ new: 0, in_progress: 1, done: 2, updated: 3, declined: -1}
+		{ 
+			new: 0,
+			processing: 1,
+			tarification: 2, 
+			negotiation: 3,
+			waiting_for_payment: 4,
+			in_progress: 5,
+		 	done: 6,
+		 	waiting_for_raiting: 7,
+		 	closed: 9, 
+		 	declined: -1
+		}
 	end
 
 	def self.description
-		{ new: "новый", in_progress: "в обработке", done: "выполнен", updated: "обновлен", declined: "отклонен"}
+		{ 
+			new: "новый",
+			processing: "обработка",
+			tarification: "тарификация", 
+			negotiation: "согласование",
+			waiting_for_payment: "ожидание оплаты",
+			in_progress: "в работе",
+		 	done: "исолпнен",
+		 	waiting_for_raiting: "ожидает рейтинга",
+		 	closed: "завершен", 
+		 	declined: "отклонен"
+		}
 
 	end
 end
