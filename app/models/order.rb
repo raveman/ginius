0f4966b42221d07a8d10d2ab6018fb11e3e6a10d
bob ershov <raveman@raveman.ru>
2015-03-25 19:19:34 +0300

    Order model: new method for formatting creation time and date

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,8 @@ class Order < ActiveRecord::Base
 	belongs_to :operator, class_name: "User"
 
 	# TODO add validations
-	
+	def date_and_time_created
+		self.created_at.strftime("%d.%m.%Y в %H:%M")
+	end	
 
 end
