358047449d8c1f3c126f98380220e85172c417bb
bob ershov <raveman@raveman.ru>
2015-03-26 17:13:15 +0300

    Users: added some localizations, added confirmations to user registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-	devise :database_authenticatable, :registerable,
+	devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_and_belongs_to_many :role
 	has_many :order
