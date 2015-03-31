c2be26a21d111bdcaed4d6006440820ae8163d6f
bob ershov <raveman@raveman.ru>
2015-03-31 12:43:24 +0300

    Fixed add confirmable migration to user model

--- db/schema.rb
+++ db/schema.rb
@@ -94,8 +94,13 @@ ActiveRecord::Schema.define(version: 20150331091707) do
     t.string   "phone",                  limit: 255
     t.string   "address",                limit: 255
     t.text     "comment",                limit: 65535
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
