b6f79f5b15fe93b661136349c8059b044591b4ec
bob ershov <raveman@raveman.ru>
2015-06-08 12:35:08 +0300

    added user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :profiles
 	resources :orders
   # devise_for :users
 	devise_for :users, path: "user", path_names: { 
--- db/migrate/20150608093432_create_profiles.rb
+++ db/migrate/20150608093432_create_profiles.rb
@@ -4,8 +4,8 @@ class CreateProfiles < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :phone
-      t.string :address
-      t.string :comment
+      t.string :email
+      t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
