72e9666a6ab9adb36e67aaee9f2bb203ccd8ed1a
bob ershov <raveman@raveman.ru>
2015-06-09 16:38:31 +0300

    after order - redirecting to root page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -54,7 +54,7 @@ class OrdersController < ApplicationController
 
 		respond_to do |format|
 			if @order.save
-				format.html { redirect_to root_path, notice: 'Ваш заказ принят!' }
+				format.html { redirect_to root_path, notice: 'Ваш заказ принят! Ожидайте звонка оператора.' }
 				format.json { render :show, status: :created, location: @order }
 			else
 				format.html { render :new }
